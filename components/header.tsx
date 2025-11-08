6b45ff46c9e167492e8d711b763ef476d31fee86
ASHUTOSH-KUMAR-RAO <icoashutosh@gmail.com>
2025-11-08 19:39:38 +0530

    13:recording-page

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,7 @@ import { ICONS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import DropdownList from "./drop-down-list";
+import RecordScreen from "./record-screen";
 
 const Header = ({ subHeader, title, userImg }: SharedHeaderProps) => {
   return (
@@ -32,12 +33,7 @@ const Header = ({ subHeader, title, userImg }: SharedHeaderProps) => {
             />
             <span>Upload a Video</span>
           </Link>
-          <div className="record">
-            <button className="primary-btn">
-              <Image src={ICONS.record} alt="record" width={16} height={16} />
-              <span>Record a Video</span>
-            </button>
-          </div>
+          <RecordScreen/>
         </aside>
       </section>
       <section className="search-filter">
